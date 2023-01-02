f7f33bd30e42dfb09165767e1a352463ed130c9b
Himan1999 <cu.16bcs1194@gmail.com[B>
2023-01-02 07:39:21 +0530

    add one more input tag and console both in console

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
 });
 
 app.post('/product', (req, res, next) => {
