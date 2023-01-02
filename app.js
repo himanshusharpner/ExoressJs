34e7aa2028de2abbbe0d5f9e566d4f50a6b88e81
Himan1999 <cu.16bcs1194@gmail.com[B>
2023-01-02 08:34:56 +0530

    shop and admin routes

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+ app.get('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
+  });
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not Found!</h1>');
--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+  res.send('<h1>Hello from Shop!</h1>');
 });
+
+
 
 module.exports = router;
